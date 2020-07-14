76a237b29bca359f7455a34abc8c7f770e8f57e3
Ilkin Fleydanli <i.fleydanli@gmail.com>
2020-07-14 12:19:19 +0300

    Project init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+import Antd from 'ant-design-vue';
 import App from './App.vue'
+import VueMapbox from "vue-mapbox";
+import Mapbox from "mapbox-gl";
+import './assets/app.css';
+
+Vue.use(VueMapbox, {mapboxgl: Mapbox});
+
+import 'ant-design-vue/dist/antd.css';
 
 Vue.config.productionTip = false
+
+Vue.use(Antd);
 
 new Vue({
   render: h => h(App),
